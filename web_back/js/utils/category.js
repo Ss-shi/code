e3ec4d93ca20ccf986e8a26ff722e01a502c0258
fanyoufu <fanyoufu@outlook.com>
2019-11-21 11:17:51 +0800

    文章类别-添加

--- web_back/js/utils/category.js
+++ web_back/js/utils/category.js
@@ -4,8 +4,18 @@ var category = {
     // 获取文章类型数据
     show: function (callback) {
         
-        $.get(baseUrl + 'admin/category_search',function (res){
+        $.get(APIURLS.category_show,function (res){
             callback(res);
+        })
+    },
+    del: function(id, callback){
+        $.post(APIURLS.category_del,{id: id},function(res){
+            callback(res)
+        })
+    },
+    add: function(name,slug, callback){
+        $.post(APIURLS.category_add,{name:name,slug:slug},function(res){
+            callback(res)
         })
     }
 }
