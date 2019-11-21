ffe5f44de8987a411a45357f0ee4aeec1ba2f764
fanyoufu <fanyoufu@outlook.com>
2019-11-21 12:33:24 +0800

    格式化小部分代码

--- web_back/js/utils/config.js
+++ web_back/js/utils/config.js
@@ -3,14 +3,14 @@ var baseUrl = 'http://localhost:8000/'
 
 // 把所有要用到的接口地址进行集中管理
 var APIURLS = {
-    user_login:baseUrl + 'admin/login',
-    user_logout:baseUrl + 'admin/logout',
-    user_getInfo:baseUrl + 'admin/getuser',
+    user_login: baseUrl + 'admin/login',
+    user_logout: baseUrl + 'admin/logout',
+    user_getInfo: baseUrl + 'admin/getuser',
 
     category_show: baseUrl + 'admin/category_search',
     category_del: baseUrl + 'admin/category_delete',
     category_add: baseUrl + 'admin/category_add',
     category_edit: baseUrl + 'admin/category_edit'
-}
 
+}
 
