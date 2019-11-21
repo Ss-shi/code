565f87e1884fb459a9674a45d99242794ae3b71e
fanyoufu <fanyoufu@outlook.com>
2019-11-21 10:19:36 +0800

    文章类别-显示

--- web_back/js/utils/category.js
+++ web_back/js/utils/category.js
@@ -4,7 +4,7 @@ var baseUrl = 'http://localhost:8000/'
 var category = {
     // 获取文章类型数据
     show: function (callback) {
-        alert('ajax拿数据')
+        
         $.get(baseUrl + 'admin/category_search',function (res){
             callback(res);
         })
