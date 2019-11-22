b48300d7997cf016d984d41c16f79d05da4365a0
fanyoufu <fanyoufu@outlook.com>
2019-11-22 10:18:33 +0800

    文章列表-分页-获取分页数据

--- web_back/js/utils/article.js
+++ web_back/js/utils/article.js
@@ -1,8 +1,9 @@
 var article = {
     // callback 是调用article.show时传入的一个函数
     // 用这个函数来做具体的处理数据的工作
-    show: function ( callback ) {
-        $.get(APIURLS.article_show,function(res){
+    show: function (obj, callback ) {
+        console.log("article.show的参数",obj)
+        $.get(APIURLS.article_show,{page:obj.page},function(res){
             callback(res)
         } )
     }
