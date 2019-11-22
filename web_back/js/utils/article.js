e5eeb3b9510c35ed5ba41ee213e601a37af943d5
fanyoufu <fanyoufu@outlook.com>
2019-11-22 11:04:16 +0800

    文章列表-筛选-处理-状态

--- web_back/js/utils/article.js
+++ web_back/js/utils/article.js
@@ -1,10 +1,17 @@
 var article = {
     // callback 是调用article.show时传入的一个函数
     // 用这个函数来做具体的处理数据的工作
-    show: function (obj, callback ) {
-        console.log("article.show的参数",obj)
-        $.get(APIURLS.article_show,{page:obj.page},function(res){
-            callback(res)
-        } )
+    show: function (obj, callback) {
+        console.log("article.show的参数", obj)
+        $.get(APIURLS.article_show,
+            {
+                page: obj.page,
+                type: obj.type,
+                state: obj.status
+            },
+            function (res) {
+                callback(res)
+            }
+        )
     }
 }
--- web_back/js/utils/user.js
+++ web_back/js/utils/user.js
@@ -3,31 +3,31 @@
 var user = {
     // 接收三个参数
     // 去发请求，请求回来做什么？ 执行你传入回调函数
-    login:function(userName,userPassword, callback){
+    login: function (userName, userPassword, callback) {
         // 发 ajax请求去检查正确与否
-        console.log( '发ajax请求去检查正确与否',userName,userPassword)
+        console.log('发ajax请求去检查正确与否', userName, userPassword)
         // $.post(baseUrl+'admin/login', 
-        $.post(APIURLS.user_login, 
+        $.post(APIURLS.user_login,
             {
                 user_name: userName,
                 password: userPassword
             },
             // callback(res)
-            function(res){
+            function (res) {
                 callback(res)
             }
         )
     },
     // 参数：当请求成功之后要调用的函数
-    logout: function ( callback ){
+    logout: function (callback) {
         // 调用接口
-        $.post(APIURLS.user_logout,function(res){
-            callback(res)   
+        $.post(APIURLS.user_logout, function (res) {
+            callback(res)
         })
     },
-     
-    getInfo:function( callback ){
-        $.get(APIURLS.user_getInfo,function(res){
+
+    getInfo: function (callback) {
+        $.get(APIURLS.user_getInfo, function (res) {
             callback(res)
         })
     }
